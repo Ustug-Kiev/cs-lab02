cd6fa81cff997376672d74fda111b6178b62005c
Alice (ProtasovAA) <Protasov@AA.com>
2022-03-20 16:04:32 +0300

    code: добавлен удобной вывод результата

--- project/main.cpp
+++ project/main.cpp
@@ -7,7 +7,7 @@ int main()
     cout << "Enter A and B:" << endl;
     int a, b;
     cin >> a >> b;
-    cout << a+b << endl;
-    cout << a-b << endl;
+    cout << "A + B = " << a+b << '\n'
+         << "A - B = " << a-b << '\n';
     return 0;
 }
